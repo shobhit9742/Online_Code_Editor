4ffdddbd1f2a14e6984ffe7e5c27f7f94519a2e8
avanilodhi <avanilodhi29@gmail.com>
2024-08-31 00:56:49 +0530

    Add language selection and CodeMirror integration with Redux state management for code editor.

--- client/src/redux/slices/compilerSlice.ts
+++ client/src/redux/slices/compilerSlice.ts
@@ -1,16 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction } from "@reduxjs/toolkit/react";
 
-const initialState = {
+export interface compilerSliceStateType {
+    html: string;
+    css: string;
+    javascript: string;
+    currentLanguage: "html" | "css" | "javascript";
+}
+
+const initialState: compilerSliceStateType = {
     html: "",
     css: "",
     javascript: "",
+    currentLanguage: "html"
 };
 
 const compilerSlice = createSlice({
     name: "compilerSlice",
     initialState,
-    reducers: {}
+    reducers: {
+        updateCurrentLanguage: (state, action: PayloadAction<compilerSliceStateType["currentLanguage"]>
+
+        ) => {
+            state.currentLanguage = action.payload;
+        }
+    },
 
 });
 
-export default compilerSlice.reducer
+export default compilerSlice.reducer;
+export const { updateCurrentLanguage } = compilerSlice.actions;

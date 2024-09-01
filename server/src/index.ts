c900d837567d66c7f1e0139d4e8edbb266817da9
Shobhit Gupta <shobhitgupta0602@gmail.com>
2024-09-01 21:02:17 +0530

    Staging Change

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,16 +3,22 @@ import cors from "cors";
 import { config } from "dotenv";
 import dbConnect from "./lib/dbConnect";
 import { compilerRouter } from "./routes/compilerRoute";
+import { userRouter } from "./routes/userRoute";
 const app = express();
 
 config();
 dbConnect();
 
 app.use(express.json());
-app.use(cors());
-
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173", process.env.CLIENT_URL!],
+  })
+);
 // Routes
 app.use("/compiler", compilerRouter);
+app.use("/user", userRouter);
 
 const port = process.env.PORT || 5000;
 
